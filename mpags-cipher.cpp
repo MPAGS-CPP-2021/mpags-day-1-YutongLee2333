d9ec7c5b4f5bdfba3b1ee74d46e04807e58d23b4
Yutong Li <yutong.li@bristol.ac.uk>
2021-10-13 11:12:54 +0000

    only flow control

---
+++ mpags-cipher.cpp
@@ -0,0 +1,89 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+int main(int argc, char* argv[])
+{   
+    //This is a comment
+    /*This is a 
+    Multiline comment */
+    
+    for (size_t i(0); i< )
+    const std::vector<std::string> cmdLineArgs { argv, argv+argc};
+
+    char in_char('x');
+    
+    for(int i{1}; 1< cmdLineArg) 
+// Take each letter from user input and in each case:
+    char in_char('x');
+    while(std::cin >> in_char)
+    {
+        //Loop until the user presses Enter than then Ctrl+D
+        std::cout<<"transliteration the input,Loop unitl user presser Enter then Ctrl+D"<<std::endl;
+
+        //Convert to upper case
+        if(islower(in_char))
+        {
+            in_char = toupper(in_char);
+            return in_char
+        }
+
+        //Change numbers to words
+        if (isalnum(in_char))
+        {
+            switch(in_char)
+            {
+                case '0':
+                    in_char.find[0] == "ZERO"
+                case '1':
+                    return "ONE"
+                case '2':
+                    return "TWO"
+                case '3':
+                    return "THREE"
+                case '4':
+                    return "FOUR"
+                case '5':
+                    return "FIVE"
+                case '6':
+                    return "SIX"
+                case '7':
+                    return "SEVEN"
+                case '8':
+                    return "EIGHT"
+                case '9':
+                    return "NINE"
+            }
+            
+        }
+
+        // Convert to upper case
+        char toUpper(char in_char)
+        {if (in_char >= 'a' && in_char <= 'z')
+            return in_char - 'a' + 'A';
+        else
+            return in_char;
+        }
+        //Convert to upper case
+       for (char& c : in_char) {
+            c = std::toupper(c, std::locale());
+        }
+        //-========
+
+
+        ---------------------
+
+
+    }
+
+  
+
+
+
+    
+    
+
+
+    
+    
+}
